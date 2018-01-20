35664d7a28b8a00e463c8f6cca98731c4ca4b68b
juliahou8 <jjhou@andrew.cmu.edu>
2018-01-20 10:16:57 -0500

    Added function skeletons

--- meow.py
+++ meow.py
@@ -9,7 +9,21 @@ for submission in subreddit.rising(limit=30):
 	print str(datetime.utcfromtimestamp(submission.created_utc)) + " " + submission.title + " " + str(submission.ups)
 print datetime.utcnow()
 
+sr = ['memes']
+
+# goes through list of strings and creates subreddit instance for each
+def meow(l):
+
+# goes through list of subreddit instances and gets top rising posts and their
+# created time and their upvote number, as well as the subreddit size
+def woof(l):
+
+# gives score to a post based on created time, upvotes, and subreddit size
 def score(created, ups, sr_size):
 	return 1
+
+# gives score to each subreddit instance in l, sorts and takes top ones
+def quack(l):
+
 
 #lookup on origin?

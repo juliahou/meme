daf2390425794984ddfb49b0ab375c2aabf226ed
Ruyee Frank <ruyeefrank@gmail.com>
2018-01-20 11:31:32 -0500

    added post attributes functionality

--- meow.py
+++ meow.py
@@ -5,25 +5,37 @@ reddit = praw.Reddit(client_id='rueV3_rhsdbfgg',
                      client_secret='45N-HS2wbzk1-AlWLolb-St2g7M',
                      user_agent='Python:com.meme:v1.0 (by /u/Leafwing)')
 subreddit = reddit.subreddit('memes')
-for submission in subreddit.rising(limit=30):
-	print str(datetime.utcfromtimestamp(submission.created_utc)) + " " + submission.title + " " + str(submission.ups)
+for submission in subreddit.rising(limit=1):
+    print str(datetime.utcfromtimestamp(submission.created_utc)) + " " + submission.title + " " + str(submission.ups)
+    print vars(submission)
+    print submission.subreddit.subscribers
 print datetime.utcnow()
 
-sr = ['memes']
+sr = ['memes', 'dankmemes']
+instances = []
 
 # goes through list of strings and creates subreddit instance for each
-def meow(l):
+def initialize_subreddits(sub_names):
+    for sub in sr:
+        instances.append(reddit.subreddit(sub))
 
 # goes through list of subreddit instances and gets top rising posts and their
 # created time and their upvote number, as well as the subreddit size
-def woof(l):
+def get_rising_post_attributes(instances):
+    posts = []
+    for inst in instances:
+        posts.extend(inst.rising(limit=3))
+    postToAttributes = {}
+    for post in posts:
+        postToAttributes[post] = (post.created_utc, post.ups, post.subreddit.subscribers)
+
 
 # gives score to a post based on created time, upvotes, and subreddit size
 def score(created, ups, sr_size):
 	return 1
 
-# gives score to each subreddit instance in l, sorts and takes top ones
+# gives score to each post in l, sorts and takes top ones
 def quack(l):
-
+    return
 
 #lookup on origin?

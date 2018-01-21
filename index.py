91324eca5c86406d25118bfc32ce3effb119a3da
juliahou8 <jjhou@andrew.cmu.edu>
2018-01-20 19:58:31 -0500

    did i change anything?

--- index.py
+++ index.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import meow
 app = Flask(__name__)
+app.jinja_env.add_extension('jinja2.ext.loopcontrols')
 
 @app.route("/")
 def show_data():
-	data = meow.main()
+	data = meow.main(6)
 	return render_template('index.html', data=data)
--- meow.py
+++ meow.py
@@ -1,6 +1,10 @@
 import praw
 from datetime import datetime
 from operator import itemgetter
+from dateutil import tz
+
+from_zone = tz.gettz('UTC')
+to_zone = tz.gettz('America/New_York')
 
 #from json.json_util import dumps
 
@@ -9,11 +13,12 @@ reddit = praw.Reddit(client_id='rueV3_rhsdbfgg',
                      user_agent='Python:com.meme:v1.0 (by /u/Leafwing)')
 
 subreddit = reddit.subreddit('memes')
-for submission in subreddit.rising(limit=1):
+'''for submission in subreddit.rising(limit=1):
+	print vars(submission)
     print str(datetime.utcfromtimestamp(submission.created_utc)) + " " + submission.title + " " + str(submission.ups)
     print vars(submission)
     print submission.subreddit.subscribers
-print datetime.utcnow()
+print datetime.utcnow()'''
 
 sr = ['memes', 'dankmemes']
 
@@ -30,7 +35,7 @@ def initialize_subreddits(sub_names):
 def get_rising_post_attributes(instances):
     posts = []
     for inst in instances:
-        posts.extend(inst.rising(limit=3))
+        posts.extend(inst.rising(limit=5))
     postToAttributes = {}
     for post in posts:
         postToAttributes[post] = (datetime.utcfromtimestamp(post.created_utc), post.ups, post.subreddit.subscribers)
@@ -38,32 +43,37 @@ def get_rising_post_attributes(instances):
 
 # gives score to a post based on created time, upvotes, and subreddit size
 def score(created, ups, sr_size):
-    now = datetime.now()
-    print created
+    now = datetime.utcnow()
     elapsed_time = now - created
     elapsed_time_minutes = elapsed_time.total_seconds()/60
     if(elapsed_time_minutes > 120):
-        return 1.0 * ups / sr_size
+        return 1.0 * ups / sr_size * 1000000
     else:
-	   return 1.0 * ups / sr_size / (1.0 * elapsed_time_minutes / 120)
+	   return 1.0 * ups / sr_size / (1.0 * elapsed_time_minutes / 120) * 1000000
 
 # gives score to each post in d, sorts and takes top ones
 def score_sort(d):
     result_list = []
     for key, value in d.iteritems():
         result_list.append((key, score(value[0],value[1],value[2])))
-    return sorted(result_list, key=itemgetter(1))
+    result_list = sorted(result_list, key=itemgetter(1))
+    result_list.reverse()
+    print result_list
+    return result_list
 
-def test(l):
+def test(l, n):
 	ans = []
-	for sub in l:
+	for sub in l[:n]:
 		submission = sub[0]
-		ans.append([datetime.utcfromtimestamp(submission.created_utc), submission.title, submission.ups, sub[1], submission.preview["images"][0]["source"]["url"]])
+		utc = datetime.utcfromtimestamp(submission.created_utc)
+		utc = utc.replace(tzinfo=from_zone)
+		est = utc.astimezone(to_zone)
+		ans.append([est, submission.title, sub[1], "http://reddit.com"+str(submission.permalink), submission.preview["images"][0]["source"]["url"]])
 	return ans
 
-def main():
+def main(n):
 	# d goes from post to attributes
 	d = get_rising_post_attributes(initialize_subreddits(sr))
-	return test(score_sort(d))
+	return test(score_sort(d), n)
 
 #lookup on origin?
